f30e921122d12cf36909a6c8139f9d19f51d35f4
isadamu <longruim@qq.com>
2021-01-10 17:39:47 +0800

    update

--- callback.h
+++ callback.h
@@ -1,4 +1,4 @@
 
-void GoTsCallBackForC(char* taskId, char* filePath, double tsBegineTime, double tsEndTime);
+void GoTsCallBackForC(char* taskId, char* filePath, double tsBeginTime, double tsEndTime);
 
 void GoSnapCallBackForC(char* taskId, char* filePath, double snapTime);
